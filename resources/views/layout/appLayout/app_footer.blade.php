4c97b3ddae3d6b26bbde5f424f24b7b4abec3be5
Abilandou <godloveabilandou@gmail.com>
2019-09-22 00:46:33 +0100

    Finish setting up project with sqlite, the custom admin template

---
+++ resources/views/layout/appLayout/app_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>@goddyTech</b>
+    </div>
+    <strong>Copyright &copy; <?php echo Date('Y'); ?> <a href="#">Student Record</a>.</strong> All rights
+reserved.
+</footer>
